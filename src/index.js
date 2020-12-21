dbfad50b404fbdf91b07b367742595097bef4514
Aleksandr Magdich <alexkotorij@gmail.com>
2020-12-21 15:25:59 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import * as $ from 'jquery'
 import Post from '@models/Post'
-import './styles/style.css'
-import json from './assets/json'
-import xml from './assets/data.xml'
-import csv from './assets/data.csv'
+// import json from './assets/json'
+// import xml from './assets/data.xml'
+// import csv from './assets/data.csv'
 import Logo from './assets/webpack-logo.png'
+import './babel'
+import './styles/style.css'
+import './styles/style.scss'
+
 
 
 const post = new Post("webpack post title", Logo)
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,173 @@
-const path = require("path")
-const HTMLWebpackPlugin = require("html-webpack-plugin")
-const {CleanWebpackPlugin} = require("clean-webpack-plugin")
-const CopyWebpackPlugin = require("copy-webpack-plugin")
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
-const isDev = process.env.NODE_ENV === "development"
-const isProd = !isDev
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
 
+const optimization = () => {
+	const config = {
+		splitChunks: {
+			chunks: "all",
+		},
+	};
 
-console.log("isDev", isDev)
-module.exports = {
-    mode: "development",
-    context: path.resolve(__dirname, "src"),
-    entry: {
-        main: "./index.js",
-        analytics: "./analytics.js",
-    },
-    output:{
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "dist")
-    },
-    resolve:{
-        alias:{
-            '@models': path.resolve(__dirname, 'src/models'),
-            '@': path.resolve(__dirname, 'src'),
-        }
-    },
-    optimization:{
-        splitChunks:{
-            chunks: 'all'
-        }
-    },
-    devServer:{
-        port: 4200,
-        open:true,
-        hot:isDev
-    },
-    plugins:[
-        new HTMLWebpackPlugin({
-            template:"./index.html",
-            minify:{
-                collapseWhitespace: isProd
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns:[
-                {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
-                }
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css",
-        })
-    ],
-    module:{
-        rules:[
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader:MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: path.resolve(__dirname, "dist"),
-                            hmr: isDev,
-                            reloadAll: true
-                        },
-                    }, "css-loader"]
-            },
-            {
-                test:/\.(png|jpg|svg)$/,
-                use: ["file-loader"]
-            },
-            {
-                test:/\.(ttf|woff|woff2|eot)$/,
-                use: ["file-loader"]
-            },
-            {
-                test:/\.xml$/,
-                use: ["xml-loader"]
-            },
-            {
-                test:/\.csv$/,
-                use: ["csv-loader"]
-            }
-        ]
+	if (isProd) {
+		config.minimizer = [
+			new OptimizeCssAssetWebpackPlugin(),
+			new TerserWebpackPlugin(),
+		];
+	}
+
+	return config;
+};
+
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+const cssLoaders = (extra) => {
+	const loaders = [
+		{
+			loader: MiniCssExtractPlugin.loader,
+			options: {
+				hmr: isDev,
+				reloadAll: true,
+                publicPath: "",
+                // publicPath: (resourcePath, context) => {
+                //     return path.relative(path.dirname(resourcePath), context) + '/';
+                // }
+			},
+		},
+		"css-loader",
+	];
+
+	if (extra) {
+		loaders.push(extra);
+	}
+
+	return loaders;
+};
+const babelOptions = (preset) => {
+	const opts = {
+		presets: ["@babel/preset-env"],
+		plugins: ["@babel/plugin-proposal-class-properties"],
+	};
+
+	if (preset) {
+		opts.presets.push(preset);
+	}
+
+	return opts;
+};
+
+const jsLoaders = () => {
+	const loaders = [
+		{
+			loader: "babel-loader",
+			options: babelOptions(),
+		},
+	];
+
+	if (isDev) {
+		loaders.push("eslint-loader");
+	}
+
+	return loaders;
+};
+
+const plugins = () =>{
+    const base = [
+		new HTMLWebpackPlugin({
+			template: "./index.html",
+			minify: {
+				collapseWhitespace: isProd,
+			},
+		}),
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, "src/favicon.ico"),
+					to: path.resolve(__dirname, "dist"),
+				},
+			],
+		}),
+		new MiniCssExtractPlugin({
+			filename: filename("css"),
+		}),
+    ]
+    if(isProd) {
+        base.push(new BundleAnalyzerPlugin())
     }
+    return base
 }
+const sourceMap = () => {
+    if (isDev) {
+        return 'source-map';
+    }
+};
+
+module.exports = {
+	context: path.resolve(__dirname, "src"),
+	mode: "development",
+	entry: {
+		main: ["@babel/polyfill", "./index.js"],
+		analytics: "./analytics.js",
+	},
+	output: {
+		filename: filename("js"),
+		path: path.resolve(__dirname, "dist"),
+	},
+	resolve: {
+        extensions: ['.js', '.json', '.png'],
+		alias: {
+			"@models": path.resolve(__dirname, "src/models"),
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
+	optimization: optimization(),
+	devServer: {
+		port: 4200,
+		open: true,
+		hot: isDev,
+    },
+    devtool: sourceMap(),
+	plugins: plugins(),
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: cssLoaders(),
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: cssLoaders("sass-loader"),
+			},
+			{
+				test: /\.(png|jpg|svg)$/,
+				use: ["file-loader"],
+			},
+			{
+				test: /\.(ttf|woff|woff2|eot)$/,
+				use: ["file-loader"],
+			},
+			{
+				test: /\.xml$/,
+				use: ["xml-loader"],
+			},
+			{
+				test: /\.csv$/,
+				use: ["csv-loader"],
+			},
+			{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            },
+		],
+	},
+};
